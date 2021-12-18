25d65c3f01f9ec7ba1de8d1bfc8777dd45d03adf
Coding Taggers <69812181+CodingTaggers@users.noreply.github.com>
2021-12-18 19:04:35 +0530

    Create SendInstaMessage.py

---
+++ SendInstaMessage.py
@@ -0,0 +1,5 @@
+from instabot import Bot
+MyInstabot = Bot()
+
+MyInstabot.login(username="Sender's Your Username", password=" Sender's Account Password")
+MyInstabot.send_message("Hello Dear", ["Receiver's Username"])
